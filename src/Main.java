30683aa7c05b238decb864f46e2ff78c8399f99c
mmopara <mmopara@gmail.com>
2022-01-11 09:42:47 +0100

    cw 6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.*;
 
 public class Main {
@@ -18,12 +20,60 @@ public class Main {
                 d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
                 elementów listy
           */
+        System.out.println("Zadanie 1a ");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj 5 nazw zwierząt: ");
+
+        List<String> lista1 = new ArrayList<>();
+        for (int i = 0; i<5;i++ ) {
+            lista1.add(scan.nextLine());
+        }
+        System.out.println(lista1);
+
+        System.out.println("\n Zadanie 1b");
+        for (String x : lista1) {
+            System.out.println(x + ",");
+        }
+
+        System.out.println("\n Zadanie 1c");
+        lista1.remove(4);
+        lista1.remove(3);
+        System.out.println(lista1);
+        System.out.println("Proszę podać 3 inne zwięrzęta, które nie znajdywały się wcześniej w liście:");
+        lista1.add(scan.nextLine());
+        lista1.add(scan.nextLine());
+        lista1.add(scan.nextLine());
+        System.out.println(lista1);
+
+        System.out.println("\n Zadanie1d");
+        lista1.sort(Collections.reverseOrder());
+        for (String x  :lista1
+        ) {
+            System.out.println(x + ",");
+        }
+
+
 
 
          /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
                 niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
                  a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
          */
+        System.out.println("\n Zadanie 2");
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("\n Proszę podać 10 liczb ( niech się powtarzaja): ");
+        Set<Integer> liczba = new TreeSet<>();
+        for (int j= 0; j<10 ; j++){
+            liczba.add(scan1.nextInt());}
+
+        List<Integer> sortedList = new ArrayList<>(liczba);
+        Collections.sort(sortedList);
+        System.out.println(sortedList);
+        System.out.println(liczba);
+        for (Integer y: liczba) {
+            System.out.println(y + ",");
+        }
 
     }
 
