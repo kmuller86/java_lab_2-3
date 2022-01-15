0e2a8f4223b066012dc7653c6ebcd1621d17f2b0
mmopara <mmopara@gmail.com>
2022-01-15 09:37:03 +0100

    cw 1 petle while

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,21 @@ public class Main {
           to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
         */
 
+        //2 ostatnie cyfry to 24, zmienna k
 
-/*
+
+        int index = 43074;
+        int x = index % 100;
         int i = 1000;
-        int k = 24;
+        do {
+            System.out.println(i);
+            if (i % x == 0){
+                System.out.println("Liczba " + i + " jest podzielna przez " + x);
+            }
+        } while (--i > 0);
         System.out.println("\n");
-        while ((--i) > 0) {
-            if (i % 24 == 0) {
-                System.out.print("\nliczba: " + i + " - podzielna przez 24");
-            }
-            if (i == 997) {
-                System.out.print("\nliczba: " + i + "  - ten numer to kłopoty");
-            }
-        }
-*/
+
+
         /* zad 2
         a) stworzyć tablicę typu int wielkosci 5, przypisac jej dowolne wartości,
           przeiterować tablicę po elementach i wyswietlić, użyć pętli while - uzyc 2 petle
@@ -32,19 +33,58 @@ public class Main {
 
 
 //        a)
-/*        int[] tablica = new int[5];
-        for (int i = 0; i < 5; i++)
-            tablica[i] = i + 1;
-        for (int i = 0; i < 5; i++)
-            System.out.println(tablica[i]);
-    }
-}*/
+
+        int [] tablica1 = new int [5];
+        tablica1[0] = 87;
+        tablica1[1] = 65;
+        tablica1[2] = 91;
+        tablica1[3] = 43;
+        tablica1[4] = 28;
+        int j = 0;
+        while(j < 5){
+            System.out.println("table [" + j +"] = " + tablica1[j]);
+            ++j;
+            if(j == 5){
+                System.out.println("\n");
+                break;
+            }
+        }
+
         //  b)
-/*        double[] tablica = new double[5];
-        for (int d = 0; d < 5; d++) {
-            tablica[d] = d + 1.13;
-            System.out.println(tablica[d]);
-        }*/
+        double [] tablica2 = new double[5];
+        tablica2[0] = 3.14;
+        tablica2[1] = 4.12;
+        tablica2[2] = 7.67;
+        tablica2[3] = 8.916;
+        tablica2[4] = 10;
+
+        int k = 0;
+        while(k < 5){
+            System.out.println("table_double [" + k +"] = " + tablica2[k]);
+            ++k;
+            if(k == 5){
+                System.out.println("\n");
+                break;
+            }
+        }
+
+        //c
+        String [] table_String = new String[5];
+        table_String[0] = "Maciej";
+        table_String[1] = "Kamil";
+        table_String[2] = "Karolina";
+        table_String[3] = "Stefa";
+        table_String[4] = "Pola";
+
+        int s = 0;
+        while(s < 5){
+            System.out.println("table_String [" + s +"] = " + table_String[s]);
+            ++s;
+            if(s == 5){
+                System.out.println("\n");
+                break;
+            }
+        }
 
 
 
@@ -54,12 +94,11 @@ public class Main {
         */
 
 
-     /*   while (true)
-        {
+  /*      while (true) {
             System.out.println("Siemano");
         }*/
 
-        //Pętla (true) uruchamia się w nieskończoność
+        //Pętla (true) uruchamia się w nieskończoność, false się wysypuje, zakomentowałem aby móc sprawdzic inne zadania
 
     }
 }
